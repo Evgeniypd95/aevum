269a9e539999fbdae8212ae84bb08cdbb9819610
Evgeny Pavlov <evgeniypd95@gmail.com>
2017-08-06 23:59:02 +0500

    added scalability

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>Aevum</title>
 	<link rel="stylesheet" type="text/css" href="http://127.0.0.1:4567/css/custom_css.css">
 <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
